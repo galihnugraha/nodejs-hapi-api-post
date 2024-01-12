d421fe8fd3ef3b051ede0beb56e374d1d960f63e
Galih Nugraha <galihnugraha58@gmail.com>
2024-01-12 17:56:59 +0700

    Latihan: Membuat HTTP Server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-console */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
   });
-
-  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
