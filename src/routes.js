f47c1f0fdfc646769a3ccb65a58bcebbf9b5755b
Galih Nugraha <galihnugraha58@gmail.com>
2024-01-12 17:42:01 +0700

    eslint

--- src/routes.js
+++ src/routes.js
@@ -1,60 +1,52 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 const routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => 'Homepage',
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => 'Halaman tidak dapat diakses dengan method tersebut',
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => 'About page',
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => 'Halaman tidak dapat diakses dengan method',
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = 'stranger' } = request.params;
+      const { lang } = request.query;
+
+      if (lang === 'id') {
+        return `Hai, ${name}!`;
+      }
+      return `Hello, ${name}!`;
     },
-    {
-        method: '*',
-        path: '/',
-        handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
-        },
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      return `Welcome ${username}!`;
     },
-    {
-        method: 'GET',
-        path: '/about',
-        handler: (request, h) => {
-            return 'About page';
-        },
-    },
-    {
-        method: '*',
-        path: '/about',
-        handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method';
-        },
-    },
-    {
-        method: 'GET',
-        path: '/hello/{name?}',
-        handler: (request, h) => {
-            const { name = "stranger" } = request.params;
-            const { lang } = request.query;
-     
-            if(lang === 'id') {
-                return `Hai, ${name}!`;
-            }
-            return `Hello, ${name}!`;
-        },
-    },
-    {
-        method: 'POST',
-        path: '/login',
-        handler: (request, h) => {
-            const { username, password } = request.payload;
-            return `Welcome ${username}!`;
-        },
-    },
-    {
-        method: '*',
-        path: '/{any*}',
-        handler: (request, h) => {
-            return 'Halaman tidak ditemukan';
-        },
-    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => 'Halaman tidak ditemukan',
+  },
 ];
- 
-module.exports = routes
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
 
-    server.route(routes);
- 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
